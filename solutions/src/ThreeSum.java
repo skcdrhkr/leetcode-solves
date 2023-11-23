a636e4dad0814f2e25296e221b8adfad2ec7c629
skcdrhkr <skcdrhkr@github.com>
2023-11-23 20:13:42 +0530

    3Sum - Medium

---
+++ solutions/src/ThreeSum.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Problem URL: https://leetcode.com/problems/3sum/
+ * Level: Medium
+ */
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        System.out.println(ThreeSum.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(ThreeSum.threeSum(new int[]{0, 1, 1}));
+        System.out.println(ThreeSum.threeSum(new int[]{0, 0, 0}));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int i = 0, j = 1, k = n - 1;
+        if (nums[0] > 0)
+            return result;
+        while (i < n - 1) {
+
+            j = i + 1;
+            k = n - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] + nums[i] == 0) {
+                    result.add(List.of(nums[i], nums[j], nums[k]));
+                    j += 1;
+                    k -= 1;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                } else if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+            i++;
+            while (i < n - 2 && nums[i] == nums[i - 1])
+                i++;
+        }
+        return result;
+    }
+}
